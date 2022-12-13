fa1b709d707312de5f9d3884cc8a030d58928bde
PAGGALA241 <vvardhan200@gmail.com>
2022-12-13 15:00:00 +0000

    CP-3c

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (Request, Response) => {
+  Response.send("Home Page");
+});
+
+app.get("/about", (Request, Response) => {
+  Response.send("About Page");
+});
+
+module.exports = app;
